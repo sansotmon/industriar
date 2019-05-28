b627b63f2294c2daf31973381c92e38d76d38991
Santiago Soto <sansotmon@gmail.com>
2019-05-28 10:21:07 -0500

    Now use html5
    Add assets,
    add logo login and background

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
-<template lang="pug">
-  #app
-
-    .container.logo
-      img(src="/src/assets/logo.png")
-    section.section
-      h1 LOGIN
-      nav.nav.has-shadow
-        .container
-          input.input(v-model="username" type="text" placeholder="Username")
-          br
-          input.input(v-model="password" type="text" placeholder="Password")
-          br
-          a.button.btn-primary(@click="doLogin") Log in
-          br
-          p {{ printResponse }}
-          br
-          register
+<template >
+    <div class="container customfont" id="app">
+    <div class="row" style="margin-top: 7%">
+      <div class="col-12" align="center">
+        <figure>
+          <img :src="hrefLogo"/>
+          <p>IndustriAR</p>
+        </figure>
+      </div>
+    </div>
+    <div class="login">
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Username</label>
+          <input v-model="username" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="row">
+        <div class="col-12" align="right" style="margin-bottom: 5%;">
+          <a href="rememberPassword.html">Remember password</a>
+        </div>
+        <div>
+          <p>{{ printResponse }}</p>
+        </div><br>
+      </div>
+        <div align="center">
+          <input type="button" class="btn btn-primary" role="button" @click="doLogin"  name="Log in" value="Log in"></input>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -27,19 +43,24 @@ export default {
     return {
       username: '',
       password: '',
-      response: ''
+      response: '',
+      url:'src/images/logo.png'
     }
   },
 
   computed: {
     printResponse () {
       return this.response
+    },
+    hrefLogo() {
+      return this.url
     }
   },
   methods: {
     doLogin () {
       if (this.username == '' || this.password == ''){
         this.response = 'Debes llenar todos los campos' 
+        this.$router.go('register');
         return
       }
       userService.login('login',this.username,this.password)
